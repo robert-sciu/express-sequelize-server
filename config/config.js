6e13dff641beaa230d7b6d6fe7bbdaa68d72dc50
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:47:55 +0100

    some normalization

--- config/config.js
+++ config/config.js
@@ -38,7 +38,7 @@ module.exports = {
     postgres: {
       options: {
         host: process.env.RDS_HOSTNAME,
-        user: process.env.RDS_USERNAME,
+        username: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
         port: process.env.RDS_PORT,
         dialect: "postgres",
