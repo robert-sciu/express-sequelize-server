181c2c40cc4d8b5b4cd3e6195e5e3883f7f015fc
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:47:02 +0100

    tWEAK

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
-const config = require("./config")[process.env.NODE_ENV || "development"];
+// const config = require("./config")[process.env.NODE_ENV || "development"];
+const config = require("./config")["aws"];
 
 console.log(config.user);
 const sequelize = new Sequelize(config.postgres.options);
