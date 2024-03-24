5d857653d67c5671361e66676bfff940296ea323
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:54:38 +0100

    logging data

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize");
 // const config = require("./config")[process.env.NODE_ENV || "development"];
 const config = require("./config")["aws"];
 
-console.log(config.user);
+console.log(config.postgres.username);
+
 const sequelize = new Sequelize(config.postgres.options);
 
 module.exports = sequelize;
