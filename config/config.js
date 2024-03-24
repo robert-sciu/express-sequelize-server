943214489af6d1a6af0ab9e29110bb8770be0dd8
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:30:56 +0100

    reformatted config obj

--- config/config.js
+++ config/config.js
@@ -35,9 +35,13 @@ module.exports = {
     serviceTimeout: 30,
   },
   aws: {
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
+    postgres: {
+      options: {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+      },
+    },
   },
 };
