8ce6face997f431c51a88e3371a8a3c129bf7105
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:41:16 +0100

    idk

--- config/config.js
+++ config/config.js
@@ -41,6 +41,7 @@ module.exports = {
         user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
         port: process.env.RDS_PORT,
+        dialect: "postgres",
       },
     },
   },
