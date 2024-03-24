86285fa2c3ac90ef7780897312a392093c507bae
robert-sciu <robert.sciu@gmail.com>
2024-03-24 14:37:06 +0100

    test

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config")[process.env.NODE_ENV || "development"];
 
+console.log(config.user);
 const sequelize = new Sequelize(config.postgres.options);
 
 module.exports = sequelize;
