f52a5ca0891814360f847a282e6feb0f0224fd11
robert-sciu <robert.sciu@gmail.com>
2024-03-25 14:34:26 +0100

    added dotenv

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 // const config = require("./config")[process.env.NODE_ENV || "development"];
-const config = require("./config")["aws"];
+const config = require("./config")[process.env.NODE_ENV || "development"];
 
 console.log(config.postgres.options.username);
 console.log(config.postgres.options.dialect);
